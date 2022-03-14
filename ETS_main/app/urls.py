2b5d6f57390b46356e1b39c9cc40d866f676d64c
bvinayratna <bezzam.vinay@gmail.com>
2022-03-14 10:44:53 +0530

    login and register done

---
+++ ETS_main/app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.layout, name='layout'),
+    path('home/', views.home, name='homepage'),
+    path('login/', views.login_user, name ='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
+]

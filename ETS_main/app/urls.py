4fbc40755da7f1f15ea8ecc27346fe3ab2940cba
bvinayratna <bezzam.vinay@gmail.com>
2022-03-28 12:56:36 +0530

    listen-feature working

--- ETS_main/app/urls.py
+++ ETS_main/app/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -9,7 +10,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/',views.logout,name='logout'),
     path('exportcsv/',views.exportcsv),
-    path('watch/', views.watch),
-    path('play/', views.play),
-    path('listen/', views.listen)
+    path('watch/', views.watch, name='watch'),
+    path('play/', views.play, name='play'),
+    path('listen/', views.listen, name='listen')
 ]
--- ETS_main/app/views.py
+++ ETS_main/app/views.py
@@ -1,16 +1,12 @@
-from pyexpat.errors import messages
-import re
-from django import forms
 from django.shortcuts import redirect, render
 from .models import user
-from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate,login
 from sqlalchemy import create_engine
-
+import requests
 import pymysql
-
 import pandas as pd
-
+from django.conf import settings
+from isodate import parse_duration
+import requests
 
 
 # Create your views here.
@@ -77,10 +73,98 @@ def exportcsv(request):
     return redirect('homepage')
 
 def watch(request):
-    return render(request, "app/watch.html")
+    videos = []
+
+    if request.method=='POST':
+        search_url = 'https://www.googleapis.com/youtube/v3/search'
+        video_url = 'https://www.googleapis.com/youtube/v3/videos'
+
+        search_params = {
+            'part': 'snippet',
+            'q': request.POST['search'],
+            'key': settings.YOUTUBE_DATA_API_KEY,
+            'maxResults': 10,
+            'type': 'video'
+        }
+        r = requests.get(search_url, params=search_params)
+        results = r.json()['items']
+        print(results)
+        video_ids = []
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+        
+        if request.POST.get('submit')=='lucky':
+            return redirect(f'https://www.youtube.com/watch?v={ video_ids[0] }')
+        
+        video_params = {
+            'key': settings.YOUTUBE_DATA_API_KEY,
+            'part': 'snippet, contentDetails',
+            'id': ','.join(video_ids),
+            'maxResults': 10
+        }
+        r = requests.get(video_url, params=video_params)
+        results = r.json()['items']
+
+        
+        for result in results:
+            video_data={
+                'title': result['snippet']['title'],
+                'id': result['id'],
+                'duration': parse_duration(result['contentDetails']['duration']),
+                'url': f'https://www.youtube.com/watch?v={ result["id"] }',
+                'thumbnail': result['snippet']['thumbnails']['high']['url'],
+            }
+            videos.append(video_data)
+    context = {
+        'videos': videos,
+    }
+
+    return render(request, "app/watch.html", context)
+    '''return render(request, "app/watch.html")'''
 
 def listen(request):
-    return render(request, "app/listen.html")
 
+    track_ids=[]
+    tracks=[]
+
+    if request.method=='POST':
+
+
+        url = "https://spotify23.p.rapidapi.com/search/"
+
+        querystring = {"q": request.POST['search'],
+        "type":"tracks",
+        "offset":"0",
+        "limit":"10",
+        "numberOfTopResults":"10"}
+
+        headers = {
+            "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+            "X-RapidAPI-Key": "496a72d3e7msh8ba8211fb1028c7p10e26ejsn8bad8a5d21b8"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        results = response.json()['tracks']['items']
+
+        for result in results:
+            track_ids.append(result['data']['id'])
+
+        for result in results:
+            track_data={
+                'title': result['data']['name'],
+                'track_id': result['data']['id'],
+                'album': result['data']['albumOfTrack']['name'],
+                'album_id': result['data']['albumOfTrack']['id'],
+                'thumbnail': result['data']['albumOfTrack']['coverArt']['sources'][0],
+                'artist': result['data']['artists']['items'][0]['profile']['name']
+            }
+            tracks.append(track_data)
+
+    context={
+        'tracks': tracks,
+    }
+
+    return render(request, "app/listen.html", context)
 def play(request):
     return render(request, "app/play.html")

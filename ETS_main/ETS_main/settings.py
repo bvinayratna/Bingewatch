429d111e7671a918aa92ec68a1e68c88cf3a8771
B Vinay Ratna <79987281+bvinayratna@users.noreply.github.com>
2024-09-03 15:08:38 +0530

    Update settings.py

--- ETS_main/ETS_main/settings.py
+++ ETS_main/ETS_main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ga8!6za=00)8p^d$()512vey_k*imhh7pypluf8j4+z7o6j9f'
+SECRET_KEY = '*****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
